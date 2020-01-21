739cfb234e12445248c8039c50aa012b885f5cbb
bestpain <nitishranjan67@gmail.com>
2020-01-21 22:36:26 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express=require('express');
+const cors=require('cors');
+const app=express();
+const bodyParser=require('body-parser')
+const api_helper = require('./api')
+const API_KEY="beb4ede2007cd8285aba1d47a36b4045";
+const ROOT_URL=`http://api.weatherstack.com/current?access_key=${API_KEY}`;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/city/:zip',(req,res)=>{
+	let {zip}=req.params
+	api_helper.make_API_call(`${ROOT_URL}&query=${zip}`)
+    .then(response => {
+        res.json(response)
+    })
+    .catch(error => {
+        res.send(error)
+    })	
+});
+
+app.get('/cities/:ziplist',async (req,res)=>{
+	let zipList=(req.params.ziplist).split(',')
+	let response=[];
+	for(zip of zipList){
+		try{
+			let data=await api_helper.make_API_call(`${ROOT_URL}&query=${zip}`);
+			response.push(data)
+		}catch(e){
+			res.status(500).send(e)
+		}
+	}
+	res.send(response);	
+});
+
+app.listen(5000,()=>{
+	console.log('server listening on port 5000')
+})
